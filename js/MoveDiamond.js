0ef237cdd28479b09f4b40cea0bd6645c694c4ec
nullLuli <2323598470@qq.com>
2017-10-07 19:49:54 +0800

    方块颜色表

--- js/MoveDiamond.js
+++ js/MoveDiamond.js
@@ -219,6 +219,7 @@ function uiRefresh(diamond, callback) {
 	}, 200, function() {
 		$("#" + oldID).text(diamond.value);
 		var color = diamond.color(diamond.value);
+		 console.log("value: " + diamond.value + "   color: " + color);
 		$("#" + oldID).css("background-color", color);
 		if(diamond.mergeTo) {
 			delete diamondListLocal[diamond.row.toString() + diamond.colunm.toString()];
