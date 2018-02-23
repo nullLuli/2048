2927750fddbd30094166418880cc893daef8d9cc
李璐 <lilu2323598470@icloud.com>
2018-02-23 20:49:08 +0800

    设置正确index.html

--- js/Feature.js
+++ js/Feature.js
@@ -1,7 +1,7 @@
 function checkIfWin (diamondListParam) {
 	for (var key in diamondListParam) {
 		var diamond = diamondListParam[key];
-		if(parseInt(diamond.value) >= 8)
+		if(parseInt(diamond.value) >= 2048)
 		{
 			//赢了
 			return true;
